ad30366b8bba7779d1f096cda2e6acfeb61e9659
freshlydead <kanakramteke2001@gmail.com>
2024-09-17 13:23:19 +0530

    first commit

--- Demo.java
+++ Demo.java
@@ -1,8 +1,6 @@
-Class Demo {
+public class Demo {
 
-public static void (String[] args){
- System.out.println("This is my first program");
-
-
-}
+    public static void main(String[] args) {
+        System.out.println("This is my first program");
+    }
 }
